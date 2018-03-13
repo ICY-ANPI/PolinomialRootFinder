6ea6859ce8ffe68da23004102d9523d944e726ae
crisrivlop <crisrivlop@gmail.com>
2018-03-12 18:45:51 -0600

    Developer Sandbox

---
+++ test/ChancherSandbox.cpp
@@ -0,0 +1,12 @@
+//#define BOOST_TEST_MODULE Convergence
+#include <boost/test/unit_test.hpp>
+
+#include "LaGuerre.hpp"
+#include "Muller.hpp"
+
+
+BOOST_AUTO_TEST_CASE(ChancherSandbox)
+{
+	//Insert your code here!
+
+}
---
+++ test/CristianSandbox.cpp
@@ -0,0 +1,13 @@
+//#define BOOST_TEST_MODULE Convergence
+#include <boost/test/unit_test.hpp>
+
+#include "LaGuerre.hpp"
+#include "Muller.hpp"
+
+
+BOOST_AUTO_TEST_CASE(CristianSandbox)
+{
+	//Insert your code here!
+}
+
+
